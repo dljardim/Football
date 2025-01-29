e9c3c05ffa613af03edab880aa02c2789df03d54
dljardim <dljardim@outlook.com>
2025-01-28 19:06:04 -0600

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Damian Jardim on 1/28/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
